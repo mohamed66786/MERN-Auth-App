c2980f0c57c002ff55e5cef0a5f61ff451ab161b
mohamed66786 <mohamedtarek7080@gmail.com>
2023-11-12 01:12:35 +0200

    add new update

--- frontend/src/screens/Profile.jsx
+++ frontend/src/screens/Profile.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div className=" py-5">
       <Container className="d-flex justify-content-center">
         <Card className="p-5 d-flex flex-column align-items-center hero-card bg-light w-75">
-          <h1 className="text-center mb-4" style={{ color: "red" }}>
+          <h1 className="text-center mb-4" style={{ color: "red",borderBottom:"1px solid black"}}>
             Welcome {userInfo.name}
           </h1>
           <p className="text-center mb-4">
